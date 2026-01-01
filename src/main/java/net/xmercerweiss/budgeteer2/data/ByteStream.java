198ac7836c09b1cdf6ba7a12bf0be85d277651fe
Xavier Mercerweiss <mercerweissx@gmail.com>
2026-01-01 01:19:52 -0700

    Began bytestream tests

--- src/main/java/net/xmercerweiss/budgeteer2/data/ByteStream.java
+++ src/main/java/net/xmercerweiss/budgeteer2/data/ByteStream.java
@@ -1,8 +1,7 @@
 package net.xmercerweiss.budgeteer2.data;
 
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.nio.charset.*;
 
 import net.xmercerweiss.budgeteer2.util.*;
 
@@ -16,6 +15,9 @@ public class ByteStream
 {
   // Class Constants
   public static final int MAX_STR_LEN = 256;
+  public static final Charset CHARSET = StandardCharsets.UTF_8;
+
+  private static final String HEX_FMT = "0x%02X";
 
   // Static Methods
   private static NoSuchElementException noBytesError()
@@ -34,17 +36,17 @@ public class ByteStream
     data = new LinkedList<>();
   }
 
+  // Override Methods
+  @Override
+  public String toString()
+  {
+    String[] hexBytes = data.stream()
+      .map(b -> HEX_FMT.formatted(Byte.toUnsignedInt(b)))
+      .toArray(String[]::new);
+    return Arrays.toString(hexBytes);
+  }
+
   // Public Methods
-  public int size()
-  {
-    return data.size();
-  }
-
-  public boolean isEmpty()
-  {
-    return size() == 0;
-  }
-
   public Byte read()
   {
     return data.poll();
@@ -60,14 +62,96 @@ public class ByteStream
     return bytes;
   }
 
+  public void append(byte b)
+  {
+    data.add(b);
+  }
+
   public void append(int i)
   {
-    data.add((byte) i);
+    append((byte) i);
+  }
+
+  public void append(Byte[] bytes)
+  {
+    for (byte b : bytes)
+      append(b);
+  }
+
+  public void appendInt(int i)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      System.out.printf("0x%02X\n", i & 0xFF);
+      i >>>= 8;
+      append(i & 0xFF);
+    }
+  }
+
+  public void appendLong(long l)
+  {
+    byte mask = (byte) 0xFF;
+    for (int i = 0; i < 8; i++)
+    {
+      l >>>= 8;
+      append((byte) (l & mask));
+    }
+  }
+
+  public void appendString(String str)
+  {
+    int length = str.length();
+    byte[] bytes = str.getBytes(CHARSET);
+    while (bytes.length > MAX_STR_LEN)
+    {
+      str = str.substring(0, length - 1);
+      bytes = str.getBytes(CHARSET);
+    }
+    append(bytes.length);
+    for (byte b : bytes)
+      append(b);
+  }
+
+  public void prepend(byte b)
+  {
+    data.push(b);
   }
 
   public void prepend(int i)
   {
-    data.push((byte) i);
+    prepend((byte) i);
+  }
+
+  public void prepend(Byte[] bytes)
+  {
+    for (int i = bytes.length - 1; i >= 0; i--)
+      prepend(bytes[i]);
+  }
+
+  public int readInt()
+  {
+    if (size() < 4)
+      throw noBytesError();
+    int out = 0;
+    for (int i = 0; i < 4; i++)
+    {
+      out += read();
+      out <<= 8;
+    }
+    return out;
+  }
+
+  public long readLong()
+  {
+    if (size() < 8)
+      throw noBytesError();
+    long out = 0;
+    for (int i = 0; i < 8; i++)
+    {
+      out += read();
+      out <<= 8;
+    }
+    return out;
   }
 
   public String readString()
@@ -76,25 +160,26 @@ public class ByteStream
     Byte head = read();
     if (head == null || (length = Byte.toUnsignedInt(head)) > size())
       throw noBytesError();
-    StringBuilder mut = new StringBuilder();
-    while (length > 0)
-    {
-      mut.appendCodePoint(read());
-      length--;
-    }
-    return mut.toString();
+    byte[] bytes = new byte[length];
+    for (int i = 0; i < length; i++)
+      bytes[i] = read();
+    return new String(bytes, CHARSET);
   }
 
-  public void writeString(String str)
+  public int size()
   {
-    IntStream codePoints = str.codePoints().limit(MAX_STR_LEN);
-    MutableInt size = new MutableInt(0);
-    codePoints.forEach(
-      i -> {
-        size.increment();
-        append(i);
-      }
-    );
-    prepend(size.get());
+    return data.size();
+  }
+
+  public boolean isEmpty()
+  {
+    return size() == 0;
+  }
+
+  public Byte[] toArray()
+  {
+    Byte[] out = new Byte[size()];
+    data.toArray(out);
+    return out;
   }
 }
--- src/test/java/net/xmercerweiss/budgeteer2/tests/ByteStreamTest.java
+++ src/test/java/net/xmercerweiss/budgeteer2/tests/ByteStreamTest.java
@@ -1,5 +1,10 @@
 package net.xmercerweiss.budgeteer2.tests;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import net.xmercerweiss.budgeteer2.data.ByteStream;
+
 
 /**
  * Unit tests of the {@link net.xmercerweiss.budgeteer2.data.ByteStream} class
@@ -8,4 +13,76 @@ package net.xmercerweiss.budgeteer2.tests;
  */
 public class ByteStreamTest
 {
+  private static final Byte[] FILLER_BYTES = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+  private static final Byte[][] VALID_BYTE_ARRAYS = {
+    {1, 2, 3, 4, 5},
+    {0},
+    {},
+    {7, 7, 7, 7, 7},
+    {72, 101, 108, 108, 111, 10},
+  };
+
+  private static final String[] VALID_STRINGS = {
+    "Hello",
+    "Again",
+    "Friend of a friend",
+    "Dollars $",
+    "Pounds £",
+    "Yen ¥",
+    "🎃",
+  };
+
+  @Test
+  void Append_WithValidArrays_ReadsExpectedOrder()
+  {
+    for (Byte[] data : VALID_BYTE_ARRAYS)
+    {
+      ByteStream bytes = new ByteStream();
+      bytes.append(FILLER_BYTES);
+      bytes.append(data);
+      assertArrayEquals(FILLER_BYTES, bytes.read(FILLER_BYTES.length));
+      assertArrayEquals(data, bytes.read(data.length));
+    }
+  }
+
+  @Test
+  void Prepend_WithValidArrays_ReadsExpectedOrder()
+  {
+    for (Byte[] data : VALID_BYTE_ARRAYS)
+    {
+      ByteStream bytes = new ByteStream();
+      bytes.prepend(FILLER_BYTES);
+      bytes.prepend(data);
+      assertArrayEquals(data, bytes.read(data.length));
+      assertArrayEquals(FILLER_BYTES, bytes.read(FILLER_BYTES.length));
+    }
+  }
+
+  @Test
+  void Append_WithInts_ReadsEqual()
+  {
+    ByteStream bytes = new ByteStream();
+    for (int i = 0; i < 10; i++)
+      bytes.appendInt(i);
+    System.out.println(bytes);
+    for (int expected = 0; expected < 10; expected++)
+    {
+      int actual = bytes.readInt();
+      assertEquals(expected, actual);
+    }
+    System.out.println(bytes);
+    System.out.println();
+  }
+
+  @Test
+  void Append_WithValidStrings_ReadsEqual()
+  {
+    for (String str : VALID_STRINGS)
+    {
+      ByteStream bytes = new ByteStream();
+      bytes.appendString(str);
+      assertEquals(str, bytes.readString());
+    }
+  }
 }
